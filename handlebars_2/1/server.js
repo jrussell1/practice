a93fbffc6f1f82c69b7e66f5d9d5fe6c5c0d964a
Jordan Russell <jordanrussell1@gmail.com>
2018-09-13 19:21:28 -0400

    installed require update server.js fixed main.handlebars

--- handlebars_2/1/server.js
+++ handlebars_2/1/server.js
@@ -7,3 +7,81 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+var animals = [
+    {
+        animalType: "dog",
+        pet: true,
+        fierceness: 4
+    }, {
+        animalType: "cat",
+        pet: true,
+        fierceness: 10
+    }, {
+        animalType: "giraffe",
+        pet: false,
+        fierceness: 4
+    }, {
+        animalType: "zebra",
+        pet: false,
+        fierceness: 8
+    }, {
+        animalType: "lion",
+        pet: false,
+        fierceness: 10
+    },
+];
+
+app.get("/dog", function(req, res) {
+    res.render("dog", animals[0]);
+});
+
+app.get("/all-pets", function(req, res) {
+
+    var data = {
+        animals: []
+    };
+
+    for (var i = 0; i < animals.length; i++) {
+        var currentAnimal = animals[i];
+        if (currentAnimal.pet) {
+            data.animals.push(currentAnimal);
+        }
+    }
+
+    res.render("index", data);
+});
+
+app.get("/all-non-pets", function(req, res) {
+
+    var data = {
+        animals: []
+    };
+
+    for (var i = 0; i < animals.length; i++) {
+        if (!currentAnimal.pet) {
+            data.animals.push(currentAnimal);
+        }
+    }
+
+    res.render("index", data);
+});
+
+app.get("/all-animals", function(req, res) {
+
+    var data = {
+        animals: []
+    };
+
+    for (var i = 0; i < animals.length; i++) {
+        if (currentAnimal.pet || !currentAnimal.pet) {
+            data.animals.push(currentAnimal);
+        }
+    }
+
+    res.render("index", data);
+});
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
