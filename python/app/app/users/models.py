775eb5f07c7dd2c91b7d8c3363500a118e6b8307
Jordan Russell <jordanrussell1@gmail.com>
2018-11-02 22:37:28 -0400

    models and connection complete to db

--- python/app/app/users/models.py
+++ python/app/app/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=256, blank=False, null=False)
+    last_name = models.CharField(max_length=256, blank=False, null=False)
+    email = models.CharField(max_length=256, blank=False, null=False)
+    age = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        """
+            This will print a nice output of a user
+        """
+        return "{0} {1} | {2}".format(self.first_name, self.last_name, self.email)
